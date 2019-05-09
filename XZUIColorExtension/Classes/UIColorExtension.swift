f2e11dfc437f86e8073f5573ba96150b7515ef8c
Xu <wowxznianshao@163.com>
2019-05-09 09:33:50 +0800

    Hex Color Fixed

--- XZUIColorExtension/Classes/UIColorExtension.swift
+++ XZUIColorExtension/Classes/UIColorExtension.swift
@@ -58,9 +58,9 @@ public extension UIColor {
         let redString = String(color[color.index(color.startIndex, offsetBy: 1)...color.index(color.startIndex, offsetBy: 2)])
         let greenString = String(color[color.index(color.startIndex, offsetBy: 3)...color.index(color.startIndex, offsetBy: 4)])
         let blueString = String(color[color.index(color.startIndex, offsetBy: 5)...color.index(color.startIndex, offsetBy: 6)])
-//        Scanner(string: redString ?? "").scanHexInt32(&red)
-//        Scanner(string: greenString ?? "").scanHexInt32(&green)
-//        Scanner(string: blueString ?? "").scanHexInt32(&blue)
+        Scanner(string: redString ?? "").scanHexInt32(&red)
+        Scanner(string: greenString ?? "").scanHexInt32(&green)
+        Scanner(string: blueString ?? "").scanHexInt32(&blue)
         let hexColor = UIColor.init(red: CGFloat(red)/255.0, green: CGFloat(green)/255.0, blue: CGFloat(blue)/255.0, alpha: 1)
         return hexColor
 
