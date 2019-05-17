ac491a1a945f3705ba500965437e9253ca5ebe09
Xu <wowxznianshao@163.com>
2019-05-17 14:06:26 +0800

    0.1.4

--- XZUIColorExtension/Classes/UIColorExtension.swift
+++ XZUIColorExtension/Classes/UIColorExtension.swift
@@ -49,19 +49,26 @@ public extension UIColor {
     
     static func hexColor(_ color: String) -> UIColor{
         
-        if color.count <= 0 || color.count != 7 || color == "(null)" || color == "<null>" {
-            return UIColor.white
+        let hexString = color.trimmingCharacters(in: .whitespacesAndNewlines)
+        let scanner = Scanner(string: hexString)
+        
+        if hexString.hasPrefix("#") {
+            scanner.scanLocation = 1
         }
-        var red: UInt32 = 0x0
-        var green: UInt32 = 0x0
-        var blue: UInt32 = 0x0
-        let redString = String(color[color.index(color.startIndex, offsetBy: 1)...color.index(color.startIndex, offsetBy: 2)])
-        let greenString = String(color[color.index(color.startIndex, offsetBy: 3)...color.index(color.startIndex, offsetBy: 4)])
-        let blueString = String(color[color.index(color.startIndex, offsetBy: 5)...color.index(color.startIndex, offsetBy: 6)])
-        Scanner(string: redString).scanHexInt32(&red)
-        Scanner(string: greenString).scanHexInt32(&green)
-        Scanner(string: blueString).scanHexInt32(&blue)
-        let hexColor = UIColor.init(red: CGFloat(red)/255.0, green: CGFloat(green)/255.0, blue: CGFloat(blue)/255.0, alpha: 1)
+        
+        var color: UInt32 = 0
+        scanner.scanHexInt32(&color)
+        
+        let mask = 0x000000FF
+        let r = Int(color >> 16) & mask
+        let g = Int(color >> 8) & mask
+        let b = Int(color) & mask
+        
+        let red   = CGFloat(r) / 255.0
+        let green = CGFloat(g) / 255.0
+        let blue  = CGFloat(b) / 255.0
+    
+        let hexColor = UIColor.init(red: red, green: green, blue: blue, alpha: 1)
         return hexColor
     }
 
